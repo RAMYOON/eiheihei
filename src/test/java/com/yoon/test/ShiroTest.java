771242cb8dbb7f079cbbe867ecbe1cdb58185d4e
yoon <yinkai1995@outlook.com>
2019-02-26 17:31:42 +0800

    shiro测试

--- src/test/java/com/yoon/test/ShiroTest.java
+++ src/test/java/com/yoon/test/ShiroTest.java
@@ -15,7 +15,9 @@ public class ShiroTest {
     @Test
     public void loginOutTest() {
         // 创建SecurityManager工厂类
-        Factory<SecurityManager> factory = new IniSecurityManagerFactory("classpath:shiro.ini");
+        // Factory<SecurityManager> factory = new IniSecurityManagerFactory("classpath:shiro.ini");
+        // Factory<SecurityManager> factory = new IniSecurityManagerFactory("classpath:shiro-realm.ini");
+        Factory<SecurityManager> factory = new IniSecurityManagerFactory("classpath:shiro-multi-realm.ini");
         // 通过SecurityManager工厂类创建SecurityManager对象
         SecurityManager securityManager = factory.getInstance();
 
